35953a88cb0d152dddcc8c2a9b7a48cc0af1712d
funa <orz.home35@gmail.com>
2024-06-08 18:39:14 +0900

    240608_ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:09:05 by miyuu             #+#    #+#             */
-/*   Updated: 2024/06/08 17:20:53 by miyuu            ###   ########.fr       */
+/*   Updated: 2024/06/08 18:35:58 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*tmp;
 	t_list	*p;
+	void	*fir;
 
 	p = NULL;
 	tmp = NULL;
@@ -23,15 +24,34 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		return (NULL);
 	if (lst != NULL)
 	{
-		p = ft_lstnew(f(lst->content));
+		fir = f(lst->content);
+		if (fir == NULL)
+		{
+			ft_lstclear(&tmp, del);
+			return (NULL);
+		}
+		p = ft_lstnew(fir);
+		if (p == NULL)
+		{
+			ft_lstclear(&tmp, del);
+			del(fir);
+			return (NULL);
+		}
 		tmp = p;
 		while (lst->next != NULL)
 		{
 			lst = lst->next;
-			p->next = ft_lstnew(f(lst->content));
+			fir = f(lst->content);
+			if (fir == NULL)
+			{
+				ft_lstclear(&tmp, del);
+				return (NULL);
+			}
+			p->next = ft_lstnew(fir);
 			if (p->next == NULL)
 			{
 				ft_lstclear(&tmp, del);
+				del(fir);
 				return (NULL);
 			}
 			p = p->next;
